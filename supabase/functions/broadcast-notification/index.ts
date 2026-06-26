9263b47fab311c7081dea7ce8d7b20ef7f3da0d5
tommysmoke <tommysmokequarto@gmail.com>
2026-06-26 18:02:37 +0200

    Update index.ts

--- supabase/functions/broadcast-notification/index.ts
+++ supabase/functions/broadcast-notification/index.ts
@@ -125,7 +125,7 @@ Deno.serve(async (req: Request) => {
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-user-id',
       },
     })
   }
